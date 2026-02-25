f9fd96b9b7a4e85119e6195a084a55349c95d715
goluba-anna <razniereg@gmail.com>
2026-02-26 00:27:23 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -5,9 +5,10 @@ from aiogram.types import Update, Message
 from aiogram.fsm.storage.memory import MemoryStorage
 from dotenv import load_dotenv
 from sqlalchemy import select
+from datetime import datetime
 
 from app.database import engine, AsyncSessionLocal
-from app.models import Base, User
+from app.models import Base, User, WorkingDay
 
 load_dotenv()
 
@@ -42,6 +43,46 @@ async def start_handler(message: Message):
             await session.commit()
 
     await message.answer("Бот работает ✅ Пользователь сохранён в базе.")
+
+
+@dp.message(F.text.startswith("/add_day"))
+async def add_working_day(message: Message):
+    admin_id = int(os.getenv("ADMIN_ID"))
+
+    if message.from_user.id != admin_id:
+        await message.answer("Нет доступа.")
+        return
+
+    try:
+        parts = message.text.split()
+        _, date_str, start_str, end_str = parts
+
+        date_obj = datetime.strptime(date_str, "%Y-%m-%d").date()
+        start_time = datetime.strptime(start_str, "%H:%M").time()
+        end_time = datetime.strptime(end_str, "%H:%M").time()
+
+        async with AsyncSessionLocal() as session:
+            existing = await session.execute(
+                select(WorkingDay).where(WorkingDay.date == date_obj)
+            )
+
+            if existing.scalar_one_or_none():
+                await message.answer("Этот день уже существует.")
+                return
+
+            new_day = WorkingDay(
+                date=date_obj,
+                start_time=start_time,
+                end_time=end_time
+            )
+
+            session.add(new_day)
+            await session.commit()
+
+        await message.answer("Рабочий день добавлен ✅")
+
+    except Exception:
+        await message.answer("Формат: /add_day 2026-03-25 10:00 18:00")
 
 
 @app.post("/webhook")
